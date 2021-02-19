340eef9671063819a62878ca9a49c3bb2ad4f3df
Lúcás Meier <cronokirby@gmail.com>
2021-02-19 18:28:45 +0100

    Make canvas fill screen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-alert('hello world!');
+function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  console.log(width, height);
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(0, 0, width, height);
+}
+
+function resize(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  canvas.width = document.body.clientWidth;
+  canvas.height = document.body.clientHeight;
+  draw(canvas, ctx);
+}
+
+// This cast will not be sound if the HTML element isn't actually a canvas
+const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+resize(canvas, ctx);
