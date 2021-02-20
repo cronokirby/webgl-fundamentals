1bff7960c0a852df52dd18dbf56a56e29ffd80c2
Lúcás Meier <cronokirby@gmail.com>
2021-02-20 11:50:08 +0100

    Refactor everything to use a nice class

--- src/index.ts
+++ src/index.ts
@@ -38,83 +38,103 @@ function createProgram(
   return program;
 }
 
-function draw(canvas: HTMLCanvasElement, gl: WebGLRenderingContext) {}
+interface Attributes {
+  a_position: number;
+}
 
-function resize(canvas: HTMLCanvasElement, gl: WebGLRenderingContext) {
-  canvas.width = document.body.clientWidth;
-  canvas.height = document.body.clientHeight;
-  draw(canvas, gl);
+interface Uniforms {
+  u_resolution: WebGLUniformLocation;
+  u_color: WebGLUniformLocation;
+}
+
+interface Buffers {
+  position: WebGLBuffer;
+}
+
+class Context {
+  private constructor(
+    private gl: WebGLRenderingContext,
+    private program: WebGLProgram,
+    private attributes: Attributes,
+    private uniforms: Uniforms,
+    private buffers: Buffers,
+  ) {}
+
+  static init(gl: WebGLRenderingContext): Context {
+    const vertShader = createShader(gl, gl.VERTEX_SHADER, vertShaderSource);
+    const fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSource);
+    const program = createProgram(gl, vertShader, fragShader);
+    const attributes = {
+      a_position: gl.getAttribLocation(program, 'a_position'),
+    };
+    const uniforms = {
+      u_resolution: gl.getUniformLocation(program, 'u_resolution'),
+      u_color: gl.getUniformLocation(program, 'u_color'),
+    };
+    const buffers = {
+      position: gl.createBuffer(),
+    };
+    return new Context(gl, program, attributes, uniforms, buffers);
+  }
+
+  private setRectangle(x: number, y: number, width: number, height: number) {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+      this.gl.STATIC_DRAW,
+    );
+  }
+
+  draw() {
+    const translation = [0, 0];
+    const width = 100;
+    const height = 30;
+    const color = [Math.random(), Math.random(), Math.random(), 1];
+
+    this.gl.viewport(0.0, 0.0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    this.gl.clear(gl.COLOR_BUFFER_BIT);
+    this.gl.useProgram(this.program);
+    this.gl.enableVertexAttribArray(this.attributes.a_position);
+    this.gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.position);
+
+    this.setRectangle(translation[0], translation[1], width, height);
+
+    this.gl.vertexAttribPointer(
+      this.attributes.a_position,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0,
+    );
+
+    this.gl.uniform2f(
+      this.uniforms.u_resolution,
+      this.gl.canvas.width,
+      this.gl.canvas.height,
+    );
+    this.gl.uniform4fv(this.uniforms.u_color, color);
+
+    this.gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
+
+  resize() {
+    this.gl.canvas.width = document.body.clientWidth;
+    this.gl.canvas.height = document.body.clientHeight;
+    this.draw();
+  }
 }
 
 // This cast will not be sound if the HTML element isn't actually a canvas
 const canvas = document.getElementById('root-canvas') as HTMLCanvasElement;
 const gl = canvas.getContext('webgl');
+const ctx = Context.init(gl);
 
-resize(canvas, gl);
-window.addEventListener('resize', () => resize(canvas, gl));
-
-const vertShader = createShader(gl, gl.VERTEX_SHADER, vertShaderSource);
-const fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShaderSource);
-const program = createProgram(gl, vertShader, fragShader);
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-const resolutionUniformLocation = gl.getUniformLocation(
-  program,
-  'u_resolution',
-);
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-gl.useProgram(program);
-gl.enableVertexAttribArray(positionAttributeLocation);
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-const size = 2;
-const type = gl.FLOAT;
-const normalize = false;
-const stride = 0.0;
-const offset = 0.0;
-gl.vertexAttribPointer(
-  positionAttributeLocation,
-  size,
-  type,
-  normalize,
-  stride,
-  offset,
-);
-gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-function randomInt(range) {
-  return Math.floor(Math.random() * range);
-}
-
-function setRectangle(
-  gl: WebGLRenderingContext,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-) {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
-
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-    gl.STATIC_DRAW,
-  );
-}
-
-const colorUniformLocation = gl.getUniformLocation(program, 'u_color');
-
-for(let i = 0; i < 50; ++i) {
-  setRectangle(gl, randomInt(canvas.width), randomInt(canvas.height), randomInt(canvas.width), randomInt(canvas.height));
-  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1.0);
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
-}
+window.addEventListener('resize', () => ctx.resize());
+ctx.resize();
